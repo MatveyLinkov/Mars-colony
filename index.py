aaed547f417a9d74a5780d96de5bcbb968fda440
Matvey <matvey23lin@gmail.com>
2022-02-11 22:50:11 +0300

    promotion_image

--- index.py
+++ index.py
@@ -13,15 +13,8 @@ def show_motto():
     return 'И на Марсе будут яблони цвести!'
 
 
-@app.route('/promotion')
-def promotion():
-    return '<br>'.join(['Человечество вырастает из детства.', 'Человечеству мала одна планета.',
-                        'Мы сделаем обитаемыми безжизненные пока планеты.', 'И начнем с Марса!',
-                        'Присоединяйся!'])
-
-
-@app.route('/image_mars')
-def image_mars():
+@app.route('/promotion_image')
+def promotion_image():
     return f'''<!doctype html>
                         <html lang="en">
                             <head>
@@ -30,9 +23,24 @@ def image_mars():
                                 <title>Привет, Марс!</title>
                             </head>
                             <body>
+                                <link rel="stylesheet" type="text/css" href="{
+                                 url_for('static', filename='css/style.css')}">
+                                <link rel="stylesheet" 
+                    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+                    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                    crossorigin="anonymous">
                                 <h1>Жди нас, Марс!</h1>
                                 <img src="{url_for('static', filename='img/mars.png')}">
-                                <p>Вот она какая, красная планета</p>
+                                <div class="alert alert-secondary" role="alert">
+                                Человечество вырастает из детства.</div>
+                                <div class="alert alert-success " role="alert">
+                                Человечеству мала одна планета.</div>
+                                <div class="alert alert-info" role="alert">
+                                Мы сделаем обитаемыми безжизненные пока планеты.</div>
+                                <div class="alert alert-warning" role="alert">
+                                И начнем с Марса!</div>
+                                <div class="alert alert-danger" role="alert">
+                                Присоединяйся!</div>
                             </body>
                         </html>'''
 
